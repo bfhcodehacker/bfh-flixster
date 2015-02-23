b2e61f665b6852514e8ae0eeb258c8e5d8c44b61
Brad Habenicht <brad.habenicht@gmail.com>
2015-02-23 21:21:57 +0000

    add and configure devise gem, setup static pages and create index.html.erb

--- Gemfile
+++ Gemfile
@@ -36,7 +36,8 @@ end
 gem 'bootstrap-sass', '~> 3.3.0'
 gem 'autoprefixer-rails' 
 
-
+#add devise gem for authentication
+gem 'devise', '>= 3.2.4'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 FlixterBfh::Application.configure do
+
+  config.action_mailer.default_url_options = { :host => 'flixter-bfh.herokuapp.com/' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
