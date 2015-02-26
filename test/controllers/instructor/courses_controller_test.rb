0a322903d9e23ab7c8248643170b9275a7c68c10
Brad Habenicht <brad.habenicht@gmail.com>
2015-02-26 19:48:04 +0000

    add validation of course information, test invalid course and format error messages

--- test/controllers/instructor/courses_controller_test.rb
+++ test/controllers/instructor/courses_controller_test.rb
@@ -30,6 +30,22 @@ class Instructor::CoursesControllerTest < ActionController::TestCase
      assert_equal 1, user.courses.count
    end
 
+   test "create not valid" do
+     user = FactoryGirl.create(:user)
+     sign_in user
+   
+     assert_no_difference 'Course.count' do
+       post :create, {:course => {
+         :title => '',
+         :description => '',
+         :cost => 0
+         }
+       }
+     end
+     assert_equal 0, user.courses.count
+     assert_response :unprocessable_entity
+   end
+
    test "show found" do 
      user = FactoryGirl.create(:user)
      sign_in user
